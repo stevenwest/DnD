d720debcb0b5053c26566ae259e0f3d517f53be7
stevenwest <steven.west@influencehealth.com>
2017-10-18 16:13:05 -0500

    Added Critical Hit Logic

--- attackDice.js
+++ attackDice.js
@@ -3,13 +3,19 @@ var abilityModifier = process.argv[2];
 var proficiencyModifier = process.argv[3];
 
 var attackRoll = function(dieSize){
-  var result = Math.ceil(dieSize*Math.random())+ parseInt(abilityModifier) + parseInt(proficiencyModifier);
+  var roll = Math.ceil(dieSize*Math.random());
+  if (roll == 20) {
+    console.log(roll);
+    console.log("Critical Hit! Roll all damage dice twice!");
+  } else {
+  var result = roll + parseInt(abilityModifier) + parseInt(proficiencyModifier);
   console.log("Rolling the die!");
   console.log(result);
   if (result >= monsterAC) {
     console.log("Your attack hit!");
   } else {
     console.log("Your attack missed!");
+    }
   }
 };
 
